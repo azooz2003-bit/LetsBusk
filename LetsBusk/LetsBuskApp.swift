65073d08493b02235e88ad42fab7f1a6bd1542fc
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-11-11 20:04:59 -0500

    Initial Commit

---
+++ LetsBusk/LetsBuskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsBuskApp.swift
+//  LetsBusk
+//
+//  Created by Abdulaziz Albahar on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsBuskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
